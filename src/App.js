843acbbed2df90b6514363d89199e69e32cd1f7f
Yuriy Putsan <putsan57@gmail.com>
2021-06-22 21:33:07 +0300

    finish first version of the site

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import './App.scss';
+import generalInfo from './content/general';
+
+import Logo from './assets/images/logo.svg';
 
 function App() {
   return (
     <div className="App">
-      Start here!
+      <img className="logo" src={Logo} alt={generalInfo.logoAltText} />
+      <h1 className="name">{generalInfo.name}</h1>
+      <ul className="socials">
+        {generalInfo.socials.map(network => (
+          <li className="socials__item">
+            <a className="socials__link" target="_blank" href={network.link} title={network.titleOnHover}>
+              <img className="socials__icon" src={network.icon} alt={network.altText} />
+            </a>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
